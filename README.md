a8b7f4985d050771c09bbb9e83439c7986d2c50f
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-10-16 00:50:35 -0400

    updated CSS

--- README.md
+++ README.md
@@ -38,6 +38,9 @@ N/A
 # License 
 MIT
 
+# Author
+Colin Nebula
+
 # Contribute
 Debugging, adding new features
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -87,9 +87,9 @@ function generatePassword() {
   function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    console.log('inside');
     passwordText.value = password;
   }
+  
   
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
